4daff5dccf7a070df0a2cedb84813c44d6cbd42d
Alison <roa.alisonn@gmail.com>
2023-03-08 09:17:47 -0300

    update

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
   return (
     <div className='navbar-container'>
      <p className='logo'>
-      <Link href="">Estuches Roa</Link>
+      <Link href="/">Inicio</Link>
      </p>
 
      <button type='button' className='cart-icon' onClick={() => setShowCart(true)}>
